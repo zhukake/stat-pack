a302c8b685dcdabfc8344089c306458c70798128
Zhukake <zhukake@icloud.com>
2023-12-13 14:14:29 +0300

    v.1.03

---
+++ deploy4new.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Обновление списков пакетов
+apt update
+
+# Полное обновление системы
+apt full-upgrade -y
+
+# Установка Docker
+apt install docker.io -y
+
+# Инициализация Docker Swarm
+docker swarm init
+
+# Клонирование репозитория
+git clone https://github.com/zhukake/stat-pack.git
+
+# Переход в директорию с файлом docker-compose.yml
+cd stat-pack/grafana-docker-stack
+
+# Развёртывание стека Docker с использованием Compose
+docker stack deploy -c docker-compose.yml monitoring
