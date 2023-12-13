6a17b8d20ff4affae7be3dfcd913a5286c20f7f7
Zhukake <zhukake@icloud.com>
2023-12-13 14:51:56 +0300

    v.1.04

--- add_node.sh
+++ add_node.sh
@@ -6,7 +6,7 @@ docker swarm init
 # Клонирование репозитория
 git clone https://github.com/zhukake/stat-pack.git
 
-# Переход в директорию с файлом docker-compose.yml
+# Переход в директорию с файлом docker-compose.yml qqq
 cd ./stat-pack
 
 # Развёртывание стека Docker с использованием Compose
