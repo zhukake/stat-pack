5d4213b01e4121bc42422ddef1d7e06226620750
Zhukake <zhukake@icloud.com>
2023-12-13 14:39:34 +0300

    v.1.04

--- add_node.sh
+++ add_node.sh
@@ -7,7 +7,7 @@ docker swarm init
 git clone https://github.com/zhukake/stat-pack.git
 
 # Переход в директорию с файлом docker-compose.yml
-cd grafana-docker-stack
+cd stat-pack
 
 # Развёртывание стека Docker с использованием Compose
 docker stack deploy -c docker-compose.yml node-exporter
